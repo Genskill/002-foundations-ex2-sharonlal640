7e428f511a5bcfee97d6a3505a6ec6eeadde76c4
sharonlal640 <84147880+sharonlal640@users.noreply.github.com>
2021-05-20 12:23:36 +0530

    Create pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+int main (void)
+{
+  float a,b,c;
+  a=get_float("side 1 ");
+  b=get_float("side 2 ");
+  c=get_float("side 3 ");
+  if (a>b&&a>c)
+  {
+    if((aa)==((bb)+(cc)))
+      printf ("Yes");
+    else
+      printf ("No");
+  }
+  else if (b>a&&b>c)
+  {
+    if((bb)==((cc)+(aa)))
+      printf ("Yes");
+    else
+      printf ("No");
+  }
+  else
+  {
+    if((cc)==((aa)+(b*b)))
+      printf ("Yes");
+    else
+      printf ("No");
+  }
+}
